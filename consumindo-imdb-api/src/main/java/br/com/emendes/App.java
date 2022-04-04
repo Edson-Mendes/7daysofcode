c30e1f7b3865acd40e3211e7f3fe95f9971c92e9
Edson Mendes <edson.luiz.mendes@hotmail.com>
2022-04-04 19:29:40 -0300

    Imprimindo o corpo da resposta no console

--- consumindo-imdb-api/src/main/java/br/com/emendes/App.java
+++ consumindo-imdb-api/src/main/java/br/com/emendes/App.java
@@ -1,13 +1,36 @@
 package br.com.emendes;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Scanner;
+
+public class App {
+
+  public static void main(String[] args) throws Exception {
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Digite sua api key:");
+    String apiKey = input.nextLine();
+
+    HttpClient client = HttpClient.newHttpClient();
+
+    HttpRequest request = HttpRequest
+        .newBuilder()
+        .uri(new URI("https://imdb-api.com/pt/API/Top250Movies/"+apiKey))
+        .GET()
+        .build();
+
+    client.sendAsync(request, BodyHandlers.ofString())
+        .thenApply(HttpResponse::body)
+        .thenAccept(s -> {
+          System.out.println(s);
+        })
+        .join();
+
+    input.close();
+  }
 }
+
